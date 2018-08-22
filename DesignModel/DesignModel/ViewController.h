e12ae98eb6934ddbc5848b92b33e5826fa580e86
rlhho <zchangwen@sina.cn>
2018-08-22 17:37:31 +0800

    创建设计模式

---
+++ DesignModel/DesignModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignModel
+//
+//  Created by ken on 2018/8/22.
+//  Copyright © 2018年 zchangwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DesignModel/DesignModel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DesignModel
+//
+//  Created by ken on 2018/8/22.
+//  Copyright © 2018年 zchangwen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
